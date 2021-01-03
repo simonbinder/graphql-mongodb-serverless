42ad7b830e253566ea5f286c16acadd46c58b1ab
Simon Binder <simon.binder@sprylab.com>
2021-01-03 18:28:25 +0100

    Add limit and skip

--- src/Collections/posts.js
+++ src/Collections/posts.js
@@ -35,13 +35,7 @@ export const posts = async (root, {blog_id, filter, first, skip}, ctx) => {
     if (!isAuthenticated) {
         Object.assign(query['$or'][0], {post_status: {$regex: `.*publish.*`}});
     }
-    const posts = await Posts.find(query);
-    if (first) {
-        posts.limit(first);
-    }
-    if (skip) {
-        posts.skip(skip);
-    }
+    const posts = await Posts.find(query).limit(first).skip(skip);
     return (posts.toArray());
 }
 
